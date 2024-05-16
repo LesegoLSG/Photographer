b7827d2922c93e830f10428a4dd18e42fd966a4f
LesegoLSG <217057620@student.uj.ac.za>
2024-05-16 01:45:42 -0500

    Added a grid of images and videos

--- photographer/src/Pages/PortfolioPage.jsx
+++ photographer/src/Pages/PortfolioPage.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React from 'react';
+import data from '../Components/Portfolio/PortfolioData';
+import PortfolioCard from '../Components/Portfolio/PortfolioCard';
+import { Link } from 'react-router-dom';
+
 
 const PortfolioPage = () => {
   return (
-    <section className="section bg-pink-200">
-    Home
-</section>
+    <section className="section pt-32">
+        <div className="h1 w-full flex justify-center items-center">
+            <h1>Portfolio</h1>
+        </div>
+        <div className='md:w-full flex flex-col justify-center items-center'>
+        <div className="w-4/5 h-auto md:px-10 grid md:grid-cols-3 gap-2  justify-items-center">
+            {
+                data.map((singleData,index) =>(
+                     <PortfolioCard key={index} singleData={singleData}/>
+                ))
+            }
+        </div>
+        <Link to={'/contact'} className="btn">Hire Me</Link>
+        </div>
+        
+    </section>
   )
 }
 
