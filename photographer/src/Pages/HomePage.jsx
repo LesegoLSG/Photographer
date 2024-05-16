9adc44995032562e06dbd55c52f89337b9beeccd
LesegoLSG <217057620@student.uj.ac.za>
2024-05-15 21:15:40 -0500

    Home page

--- photographer/src/Pages/HomePage.jsx
+++ photographer/src/Pages/HomePage.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import ProfilePicture from '../assets/ProfilePicture.jpg';
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <section className="section">
+            <div className="container mx-auto h-full relative">
+                {/* text and img wrapper */}
+                <div className="flex flex-col justify-center">
+                    {/* text */}
+                    <div className="w-full pt-36 pb-14 md:pt-0 md:pb-0 md:w-auto z-10 md:absolute flex flex-col justify-center items-center md:items-start">
+                        <h1 className="h1">
+                            Photographer <br/> & Film Maker
+                        </h1>
+                        <p className="text-[26px] md:text-[36px] font-primary mb-4 md:mb-12">South Africa,RSA</p>
+                        <Link to={'/contact'} className="btn mb-[30px]">Hire Me</Link>
+                    </div>
+                    {/* img */}
+                    <div className="flex justify-end max-h-96 md:max-h-max">
+                        <div className="relative md:-right-40 w-3/5">
+                            <img src={ProfilePicture} alt=""/>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+    </section>
   )
 }
 
